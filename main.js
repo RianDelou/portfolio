1611dd2284507eb84956cd617c0a7ded9e3ed0a6
RianDelou <riandelou63@hotmail.com>
2024-04-25 17:37:06 -0300

    feat: Portfolio and scroll logic in js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-
-
+const html = document.querySelector("html");
 const miniListaLinks = document.querySelectorAll(
     ".apresentacao__links__navegacao"
 );
 const checkBoxMusic = document.getElementById("switch");
 const textoMusica = document.getElementById("musica-titulo");
+const navBarElement = document.querySelectorAll(".cabecalho__nav__button");
+const containerHome = document.getElementById("container-home");
+const containerAboutMe = document.getElementById("container-about-me");
+const containerPortfolio = document.getElementById("container-portfolio");
 const music = new Audio("./Audios/By Your Side (melancholy ver.).mp3"); //long music
 const audio = new Audio("./Audios/audio.wav"); //short music
 
@@ -26,3 +29,23 @@ checkBoxMusic.addEventListener("change", () => {
         textoMusica.style.color = `#EFF6EE`;
     }
 });
+
+navBarElement.forEach( (element, index) => {
+    let aux = "";
+    if (index === 0) {
+        aux = containerHome;
+    } else if (index === 1) {
+        aux = containerAboutMe;
+    } else if (index === 2) {
+        aux = containerPortfolio;
+    }
+    element.addEventListener("click", event => {
+        event.preventDefault();
+        window.scrollTo({
+        top: aux.offsetTop,
+        behavior: "smooth"
+        });
+    });
+});
+
+    
